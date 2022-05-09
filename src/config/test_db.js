5d78732f7e74010483e1e27fe168004b5e421ac3
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2022-05-09 20:15:10 +0530

    cluster db used

--- src/config/test_db.js
+++ src/config/test_db.js
@@ -7,7 +7,9 @@ class connectDB {
 
 	_connect() {
 		mongoose
-			.connect(process.env.TESTMONGO, {
+			.connect(process.env.MONGO_URI, {
+				user: process.env.MONGO_USER,
+				pass: process.env.MONGO_PASS,
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
 			})
