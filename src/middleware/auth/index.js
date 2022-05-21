e5635f6ca976b399027a19e95e59e2b665446f0f
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2022-05-21 15:15:21 +0530

    clean code

--- src/middleware/auth/index.js
+++ src/middleware/auth/index.js
@@ -3,29 +3,6 @@ const User = require("../../models/userModel");
 
 const { validateToken, readProfile } = require("../../services/userService");
 
-//Token verification through Authorization header
-// const verifyToken = (req, res, next) => {
-// 	const token = req.header("Authorization").replace("Bearer ", "");
-
-// 	validateToken(token)
-// 		.then((user) => {
-// 			if (!user)
-// 				return res.status(403).json({ message: "Token is not found!" });
-
-// 			jwt.verify(token, process.env.JWT_SECRET, (error, docs) => {
-// 				if (error)
-// 					res.status(403).json({ message: "Token is not valid!" });
-// 				req.user = user;
-// 				req.userId = docs;
-// 				req.token = token;
-// 				next();
-// 			});
-// 		})
-// 		.catch((err) => {
-// 			res.status(500).json({ message: "Something went wrong!" });
-// 		});
-// };
-
 //Token Verification through cookie parser
 const verifyToken = (req, res, next) => {
 	const token = req.cookies.jwt;
